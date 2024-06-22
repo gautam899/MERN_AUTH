79d0bfd655dac2de5582796c1183bb3e7924aed8
Bhavya Gautam <gautambhavya899@gmail.com>
2024-06-22 13:17:15 +0530

    Connect to the database using mongodb

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config(); //initialize dotenv
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
+
+// Y8qdspPe4DJviegN
+// mongodb+srv://gautambhavya899:Y8qdspPe4DJviegN@mernauthentication.4xlrnsv.mongodb.net/?retryWrites=true&w=majority&appName=MernAuthentication
