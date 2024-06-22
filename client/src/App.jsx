68695e6c7d350cee1b318ae947e7d279a155734c
Bhavya Gautam <gautambhavya899@gmail.com>
2024-06-22 12:18:46 +0530

    add routes and create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
+/* eslint-disable no-unused-vars */
 
-export default function App() 
-{  
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+export default function App() {
   return (
-    <div className="">App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
- 
---
+++ client/src/pages/SignIn.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+function SignIn() {
+  return (
+    <div>SignIn</div>
+  )
+}
+
+export default SignIn
