a886cc927a5c79797fc24df20cfb61aa7796bc12
Bhavya Gautam <gautambhavya899@gmail.com>
2024-07-04 22:29:20 +0530

    Complete home and abot pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,8 +1,38 @@
-
 function About() {
   return (
-    <div>About</div>
-  )
+    <div className="px-4 py-12 max-w-2xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About
+      </h1>
+      <p className="mb-4 text-slate-700 ">
+        {" "}
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, doloribus
+        numquam commodi praesentium a aut minus. Odit excepturi fugiat culpa
+        voluptatibus quae est nemo exercitationem error animi ipsa nostrum
+        tempora ullam velit, dignissimos ex et. Accusantium minus dolores
+        molestias iusto delectus dicta ea labore ratione, inventore corporis
+        error beatae necessitatibus.
+      </p>
+      <p className="mb-4 text-slate-700 text-">
+        {" "}
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, doloribus
+        numquam commodi praesentium a aut minus. Odit excepturi fugiat culpa
+        voluptatibus quae est nemo exercitationem error animi ipsa nostrum
+        tempora ullam velit, dignissimos ex et. Accusantium minus dolores
+        molestias iusto delectus dicta ea labore ratione, inventore corporis
+        error beatae necessitatibus.
+      </p>
+      <p className="mb-4 text-slate-700">
+        {" "}
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, doloribus
+        numquam commodi praesentium a aut minus. Odit excepturi fugiat culpa
+        voluptatibus quae est nemo exercitationem error animi ipsa nostrum
+        tempora ullam velit, dignissimos ex et. Accusantium minus dolores
+        molestias iusto delectus dicta ea labore ratione, inventore corporis
+        error beatae necessitatibus.
+      </p>
+    </div>
+  );
 }
 
-export default About
+export default About;
