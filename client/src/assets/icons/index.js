29220ba8005efdc6d805f3a2cc170a4ad7ee3f7e
Bhavya Gautam <gautambhavya899@gmail.com>
2024-07-06 13:10:57 +0530

    Add the responsive navbar

---
+++ client/src/assets/icons/index.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-unused-vars */
+import hamburger from "./hamburger.svg";
+import close from "./close.svg";
+
+export { hamburger, close };
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,12 @@
 /* eslint-disable no-unused-vars */
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { hamburger, close } from "../assets/icons";
 import ThemeBtn from "./ThemeBtn";
+import { useState } from "react";
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <header className="bg-slate-200 dark:bg-slate-700">
       <nav className="flex justify-between max-w-6xl items-center px-10 mx-auto py-5 ">
@@ -13,7 +16,7 @@ export default function Header() {
           </h1>
         </Link>
         <ThemeBtn />
-        <ul className="flex gap-4">
+        <ul className="flex gap-4 max-lg:hidden">
           <Link to="/">
             <li className="font-semibold p-4 hover:text-slate-400 dark:text-white">
               Home
@@ -33,13 +36,63 @@ export default function Header() {
                 className="h-12 w-12 rounded-full object-cover"
               />
             ) : (
-              <li className="font-semibold p-4 hover:text-slate-400 dark:text-white">SignIn</li>
+              <li className="font-semibold p-4 hover:text-slate-400 dark:text-white">
+                SignIn
+              </li>
             )}
           </Link>
         </ul>
+        <div className="lg:hidden">
+          <img
+            src={hamburger}
+            alt="hamburger"
+            height={24}
+            width={24}
+            className="cursor-pointer"
+            onClick={() => setToggleMenu(!toggleMenu)}
+          />
+          <div>
+            {toggleMenu && (
+              <div className="flex justify-center items-center flex-col bg-slate-800 w-full h-full fixed top-0 left-0 text-white">
+                <ul className="flex-1 flex justify-center flex-col items-center gap-16">
+                  <img
+                    src={close}
+                    alt="close-menu"
+                    height={24}
+                    width={24}
+                    className="absolute top-[30px] right-[30px] cursor-pointer "
+                    onClick={() => setToggleMenu(!toggleMenu)}
+                  />
+                  <Link to="/">
+                    <li className="font-semibold p-4 hover:text-slate-400 dark:text-white">
+                      Home
+                    </li>
+                  </Link>
+
+                  <Link to="/about">
+                    <li className="font-semibold p-4 hover:text-slate-400 dark:text-white">
+                      About
+                    </li>
+                  </Link>
+                  <Link to="/profile">
+                    {currentUser ? (
+                      <img
+                        src={currentUser.profilePicture}
+                        alt="profile"
+                        className="h-12 w-12 rounded-full object-cover"
+                      />
+                    ) : (
+                      <li className="font-semibold p-4 hover:text-slate-400 dark:text-white">
+                        SignIn
+                      </li>
+                    )}
+                  </Link>
+                </ul>
+              </div>
+            )}
+          </div>
+        </div>
       </nav>
     </header>
   );
 }
-
-
